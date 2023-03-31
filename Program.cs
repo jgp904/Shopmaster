55a0a26701228d307a63c14f9d0720ee699dc796
jgp904 <jgp904@naver.com>
2023-03-31 10:46:48 +0900

    A#3 commit

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@
             {
                 Console.WriteLine("it is header");  //A#2
             }
-            Console.WriteLine("article");
+            Console.WriteLine("article"); 
             {
-
+                Console.WriteLine("본문");    //A#3
             }
             Console.WriteLine("footer");
             {
-
+                Console.WriteLine("푸터");    //A#3
             }
 
         }
